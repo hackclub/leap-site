1014789d4b118cc5f9d3e5e86674a7bd0352b18c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-31 21:48:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showtechicaldetails(){
   document.getElementById("subheader").innerHTML ='<a href="/" style="text-decoration: none;font-size: 24px">◄</a> Technical details'
   document.getElementById("subheader").style.marginBottom ='14px'
   document.getElementById("subheader").style.fontSize ='24px'
-  document.getElementById("text").innerHTML ='The board features: ATmega32u4, 20 digital input/output pins (of which 7 can be used as PWM outputs and 12 as analog inputs), a 16 MHz crystal oscillator, a micro USB connection, a power jack, built-in USB communication, an ICSP header, a reset button, keyboard simulation and mouse simulation. All SMD components are soldered on. The board is also open source <a href="https://github.com/kevin200617/Orpheus-Leap-Micro">here</a>'
+  document.getElementById("text").innerHTML ='The board features: a ATmega32u4, a 5V micro-USB power connection, 20 I/O pins, 7 PWM Channels, 12 Analog Inputs, 32 KB Flash Memory (of which 4 KB used for bootloader), 2.5 KB SRAM, 1 KB EEPROM, 5V Logic, 7-12V input range through the VIN pin, 40 mA output per I/O pin, built-in USB Support (no need for programmer) and can act as USB device (keyboard, mouse, game controller, etc). All SMD components are soldered on. The board is also open source <a href="https://github.com/kevin200617/Orpheus-Leap-Micro">here</a>'
   document.getElementById("get-button").style.display ="none"
   document.getElementById("tech-button").style.display ="none"
 }
